832d9612067d4bc0bf36332695c4cd554ff8a26a
ali-yousuf <aarajyousufdiu@gmail.com>
2025-11-24 23:48:33 +0600

    Setup navigation graph for calmi

---
+++ app/src/main/java/com/calmi/app/navigation/Screens.kt
@@ -0,0 +1,6 @@
+package com.calmi.app.navigation
+
+sealed class Screens(val route: String) {
+    object Splash : Screens("splash_screen")
+    object Home : Screens("home_screen")
+}
---
+++ app/src/main/java/com/calmi/app/ui/screens/SplashScreen.kt
@@ -0,0 +1,31 @@
+package com.calmi.app.ui.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.calmi.app.navigation.Screens
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(navController: NavController) {
+    LaunchedEffect(key1 = true) {
+        delay(2000)
+        navController.navigate(Screens.Home.route) {
+            popUpTo(Screens.Splash.route) {
+                inclusive = true
+            }
+        }
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Splash Screen")
+    }
+}
