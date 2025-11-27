9bc59c32a5b6ec8e83dd59f85971d101410600b8
ali-yousuf <aarajyousufdiu@gmail.com>
2025-11-27 15:32:29 +0600

    Customize the default splash screen to the app's brand color

--- app/src/main/java/com/calmi/app/MainActivity.kt
+++ app/src/main/java/com/calmi/app/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.calmi.app.navigation.NavGraph
 import com.calmi.app.ui.theme.CalmiTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
